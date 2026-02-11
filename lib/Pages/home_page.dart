67eaee566274f646c4ec4643fa108ffc25027f19
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-11 10:13:55 -0800

    .

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -12,64 +14,20 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 30, left: 20, right: 20),
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height / 3,
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40),
+            bottomRight: Radius.circular(40),
+          ),
+        ),
+        //margin: EdgeInsets.only(left: 20),
+        padding: EdgeInsets.only(top: 40, left: 20, right: 20),
         child: Column(
-          children: [
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //   children: [
-            //     Text(
-            //       "Smart Chef",
-            //       style: TextStyle(
-            //         color: Colors.amber,
-            //         fontSize: 30,
-            //         fontWeight: FontWeight.bold,
-            //       ),
-            //     ),
-            //     SizedBox(width: 20),
-            //     Image.asset(
-            //       height: 50,
-            //       width: 50,
-            //       fit: BoxFit.cover,
-            //       "images/boy.jpg",
-            //     ),
-            //     Padding(
-            //       padding: EdgeInsets.only(right: 10),
-            //       child: ClipOval(
-            //         child: Image.asset('images/boy.jpg', height: 10, width: 10),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Center horizontally
-                  children: [
-                    SizedBox(
-                      width: 110,
-                      height: 48,
-                      child: Image.asset('images/boy.jpg', fit: BoxFit.cover),
-                    ),
-                    Text('Order your favorite food!'),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: ClipOval(
-                    child: Image.asset(
-                      'images/boy.jpg',
-                      height: 60,
-                      width: 60,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [Text("hi")],
         ),
       ),
     );
