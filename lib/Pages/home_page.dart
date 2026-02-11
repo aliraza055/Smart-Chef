4ac09e5bfc9de3f4e738b5cf2254ec7dca8e5fbe
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-10 23:01:38 -0800

    building ui

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -15,19 +15,56 @@ class _HomepageState extends State<Homepage> {
         padding: EdgeInsets.only(top: 30, left: 20, right: 20),
         child: Column(
           children: [
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     Text(
+            //       "Smart Chef",
+            //       style: TextStyle(
+            //         color: Colors.amber,
+            //         fontSize: 30,
+            //         fontWeight: FontWeight.bold,
+            //       ),
+            //     ),
+            //     SizedBox(width: 20),
+            //     Image.asset(
+            //       height: 50,
+            //       width: 50,
+            //       fit: BoxFit.cover,
+            //       "images/boy.jpg",
+            //     ),
+            //     Padding(
+            //       padding: EdgeInsets.only(right: 10),
+            //       child: ClipOval(
+            //         child: Image.asset('images/boy.jpg', height: 10, width: 10),
+            //       ),
+            //     ),
+            //   ],
+            // ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // Image.network(
-                //   'https://share.google/15OjwHpiiBMti9hhk',
-                //   height: 50,
-                //   width: 50,
-                // ),
-                Text(
-                  "Smart Chef",
-                  style: TextStyle(
-                    color: Colors.amber,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Center horizontally
+                  children: [
+                    SizedBox(
+                      width: 110,
+                      height: 48,
+                      child: Image.asset('images/boy.jpg', fit: BoxFit.cover),
+                    ),
+                    Text('Order your favorite food!'),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: ClipOval(
+                    child: Image.asset(
+                      'images/boy.jpg',
+                      height: 60,
+                      width: 60,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ],
