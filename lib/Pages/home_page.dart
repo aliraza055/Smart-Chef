6ba6dd04babda5ff28973ca0366f0b125ae24c4d
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-13 11:14:35 -0800

    .

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -26,8 +26,38 @@ class _HomepageState extends State<Homepage> {
         //margin: EdgeInsets.only(left: 20),
         padding: EdgeInsets.only(top: 40, left: 20, right: 20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [Text("hi")],
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("hi Mr. Ali Raza"),
+                    Text('Checking the amazing receipes...'),
+                  ],
+                ),
+                ClipOval(
+                  child: Image.asset('images/boy.jpg', height: 60, width: 60),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(right: 40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hint: Text('Search.....'),
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
