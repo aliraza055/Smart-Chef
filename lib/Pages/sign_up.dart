d055461ffe256cde21e85add5f27b4cca01c85cc
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-22 00:33:42 +0500

    complete baisc ui of login page

--- lib/Pages/sign_up.dart
+++ lib/Pages/sign_up.dart
@@ -30,24 +30,98 @@ class _SignUpState extends State<SignUp> {
             ),
           ),
           Container(
-            width: double.infinity,
-            margin: EdgeInsets.only(top: 200, left: 40, right: 40, bottom: 40),
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(color: Colors.white),
+            margin: EdgeInsets.only(top: 220, left: 40, right: 40, bottom: 20),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.white,
+            ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Center(
+                  child: Text(
+                    'Singup page',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ),
                 Text(
-                  'Singup page',
+                  'Name',
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
                 Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.only(left: 20, right: 20),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(16),
-                    color: Colors.blueAccent.shade100,
+                    color: Colors.black12,
                   ),
                   child: TextFormField(
-                    decoration: InputDecoration(border: InputBorder.none),
+                    decoration: InputDecoration(
+                      hint: Text('Enter your name'),
+                      border: InputBorder.none,
+                    ),
                   ),
+                ),
+                Text(
+                  'Gmail',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.black12,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hint: Text('Enter your gmail'),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Password',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.black12,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hint: Text('Enter your password'),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Center(child: Text('Sinup')),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('If you dont have account!'),
+                    Text(
+                      'Singup',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
