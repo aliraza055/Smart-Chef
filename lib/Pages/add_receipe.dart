09dac0334d81fb44bdf0ee7972c43e48a33bcdf0
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-21 22:33:08 +0500

    create a add new recipe ui

--- lib/Pages/add_receipe.dart
+++ lib/Pages/add_receipe.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddReceipe extends StatefulWidget {
@@ -16,7 +17,123 @@ class _AddReceipeState extends State<AddReceipe> {
         centerTitle: true,
         title: Text('Add New Receipe'),
       ),
-      body: Column(children: [Container()]),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            Center(
+              child: DottedBorder(
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.camera_alt),
+                      SizedBox(height: 20),
+                      Text("Add Images"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // DropdownButtonFormField<String>(
+            //   decoration: InputDecoration(
+            //     labelText: "Select Country",
+            //     border: OutlineInputBorder(),
+            //   ),
+            //   items: ["Pakistan", "India", "USA", "UK"].map((value) {
+            //     return DropdownMenuItem<String>(value: value, child: Text(value));
+            //   }).toList(),
+            //   onChanged: (value) {
+            //     print("Selected: $value");
+            //   },
+            // ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.shade100,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hint: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text('Enter tile'),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.shade100,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hint: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text('Enter description '),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.shade100,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hint: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text('Enter category'),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 150,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.shade100,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hint: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text('Enter Ingreditions'),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hint: Center(child: Text('Done')),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
