e835d629a39a39c32ef9ff06a57eb8d911305677
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-21 23:42:37 +0500

    ui updating

--- lib/Pages/sign_up.dart
+++ lib/Pages/sign_up.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -11,13 +13,44 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
           Container(
             height: MediaQuery.of(context).size.height / 3,
             width: double.infinity,
-            decoration: BoxDecoration(color: Colors.blue),
-            child: Image.asset('images/boy.jpg', height: 30, width: 30),
+
+            decoration: BoxDecoration(color: Color(0xffffefbf)),
+            child: Center(
+              child: Image.asset(
+                'images/receipe1.jpg',
+                height: 130,
+                width: 130,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 200, left: 40, right: 40, bottom: 40),
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(color: Colors.white),
+            child: Column(
+              children: [
+                Text(
+                  'Singup page',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.blueAccent.shade100,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
