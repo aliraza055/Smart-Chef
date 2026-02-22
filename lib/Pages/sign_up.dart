67c3501bcf01deb7bd33055cd5181c8808a900cc
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-22 23:43:03 +0500

    add controller to textfields

--- lib/Pages/sign_up.dart
+++ lib/Pages/sign_up.dart
@@ -10,6 +10,10 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final _keyform = GlobalKey<FormState>();
+  final _nameCont = TextEditingController();
+  final _gmailCont = TextEditingController();
+  final _passwordCont = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,94 +40,109 @@ class _SignUpState extends State<SignUp> {
               borderRadius: BorderRadius.circular(16),
               color: Colors.white,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    'Singup page',
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  'Name',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  padding: EdgeInsets.only(left: 20, right: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.black12,
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hint: Text('Enter your name'),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Gmail',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  padding: EdgeInsets.only(left: 20, right: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.black12,
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hint: Text('Enter your gmail'),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Password',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  padding: EdgeInsets.only(left: 20, right: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.black12,
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hint: Text('Enter your password'),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Center(child: Text('Sinup')),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text('If you dont have account!'),
-                    Text(
-                      'Singup',
+            child: Form(
+              key: _keyform,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      'Singup page',
                       style: TextStyle(
+                        fontSize: 22,
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.blue,
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  Text(
+                    'Name',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.black12,
+                    ),
+                    child: TextFormField(
+                      controller: _nameCont,
+                      decoration: InputDecoration(
+                        hint: Text('Enter your name'),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Gmail',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.black12,
+                    ),
+                    child: TextFormField(
+                      controller: _gmailCont,
+                      decoration: InputDecoration(
+                        hint: Text('Enter your gmail'),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Password',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.black12,
+                    ),
+                    child: TextFormField(
+                      controller: _passwordCont,
+                      decoration: InputDecoration(
+                        hint: Text('Enter your password'),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.only(top: 20),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Center(child: Text('Sinup')),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text('If you dont have account!'),
+                      Text(
+                        'Singup',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
