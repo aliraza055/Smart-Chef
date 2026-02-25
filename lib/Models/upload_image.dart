2cc0e476ac2a6756960ce90495bb904168ea9311
aliraza055 <alirazamaqbool055@gmail.com>
2026-02-25 23:57:39 +0500

    add upload image model

--- lib/Models/upload_image.dart
+++ lib/Models/upload_image.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+
 class UploadImage {
-  Future<void> selectImage()async{
-    await imagePic
+  File? selectedImage;
+  Future<void> selectImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image == null) {
+      return;
+    } else {
+      selectedImage == File(image.path);
+    }
   }
 }
