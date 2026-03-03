32a87f9fb748520c2b7bbde64acc9bb37fb3c15b
aliraza055 <alirazamaqbool055@gmail.com>
2026-03-03 17:26:36 +0500

    create a userProfile page

---
+++ lib/Pages/user_info.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UserInfo extends StatefulWidget {
+  const UserInfo({super.key});
+
+  @override
+  State<UserInfo> createState() => _UserInfoState();
+}
+
+class _UserInfoState extends State<UserInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('User'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
