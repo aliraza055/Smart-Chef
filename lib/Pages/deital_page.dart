b7797fde0589986a7c62dcea9c6f2c0a544e0929
aliraza055 <alirazamaqbool055@gmail.com>
2026-03-05 23:38:05 +0500

    create detail page

---
+++ lib/Pages/deital_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DeitalPage extends StatefulWidget {
+  const DeitalPage({super.key});
+
+  @override
+  State<DeitalPage> createState() => _DeitalPageState();
+}
+
+class _DeitalPageState extends State<DeitalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [
+          
+        ],
+      ));
+  }
+}
