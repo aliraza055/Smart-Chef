1f2b426c74bfabc9f5911ec15cafb7c356580041
aliraza055 <alirazamaqbool055@gmail.com>
2026-03-08 23:34:45 +0500

    add conatienr

--- lib/Pages/deital_page.dart
+++ lib/Pages/deital_page.dart
@@ -11,14 +11,42 @@ class _DeitalPageState extends State<DeitalPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
+      body: Stack(
         children: [
           ClipRRect(
             child: Image.asset(
               'images/receipe1.jpg',
               width: double.infinity,
-              // height: 100,
+              height: 400,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: MediaQuery.of(context).size.width / 1),
+            padding: EdgeInsets.only(top: 15, left: 10, right: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+              color: Colors.blueGrey.shade100,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Lime Ulto Gmam',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('⭐'),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
