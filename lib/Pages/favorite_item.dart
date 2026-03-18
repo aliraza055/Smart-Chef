d3bb4fad52e04f9a381248ceb8e20ec0c97880c3
aliraza055 <alirazamaqbool055@gmail.com>
2026-03-18 23:38:34 +0500

    complete Ui of favotrite page

--- lib/Pages/favorite_item.dart
+++ lib/Pages/favorite_item.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_chef/Widgets/receipe_container.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -15,8 +16,43 @@ class _FavoritePageState extends State<FavoritePage> {
       appBar: AppBar(title: const Text("Favorite Recipes")),
 
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('Receipes').snapshots(),
-        builder: (context, snapshot) {},
+        stream: FirebaseFirestore.instance
+            .collection('Receipes')
+            .where('isFav', isEqualTo: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No recipes found'));
+          }
+          final recipes = snapshot.data!.docs;
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: recipes.length,
+            itemBuilder: (context, index) {
+              final data = recipes[index].data();
+              return RecipeCard(
+                image: data['image'],
+                name: data['name'],
+                description: data['description'],
+                time: data['time'].toString(),
+                likes: data['likes'].toString(),
+                isFavorite: true,
+
+                onFavoriteToggle: () {
+                  FirebaseFirestore.instance
+                      .collection('Receipes')
+                      .doc(recipes[index].id)
+                      .update({'isFav': false});
+                },
+              );
+            },
+          );
+        },
       ),
     );
   }
--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -94,7 +94,7 @@ class _HomepageState extends State<Homepage> {
                               description: data['description'],
                               time: data['time'].toString(),
                               likes: data['likes'].toString(),
-                              isFavorite: true,
+                              isFavorite: false,
 
                               onFavoriteToggle: () {
                                 FirebaseFirestore.instance
