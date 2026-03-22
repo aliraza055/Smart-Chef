e77599da5e8a0a306009d2d576f87e917be4584a
aliraza055 <alirazamaqbool055@gmail.com>
2026-03-22 23:54:25 +0500

    aligment

--- lib/Widgets/textfield_widget.dart
+++ lib/Widgets/textfield_widget.dart
@@ -3,15 +3,12 @@ import 'package:flutter/material.dart';
 class TextfieldWidget extends StatelessWidget {
   Icon prefix;
   String hintText;
-   TextfieldWidget({super.key,required this.prefix,required this.hintText});
+  TextfieldWidget({super.key, required this.prefix, required this.hintText});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      decoration: InputDecoration(
-        hint: hintText,
-        prefix:prefix
-      )
+      decoration: InputDecoration(hint: hintText, prefix: prefix),
     );
   }
 }
