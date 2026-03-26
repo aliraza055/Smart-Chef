c7bb178c81b94ecb012aadad8465e2947db73cff
aliraza055 <alirazamaqbool055@gmail.com>
2026-03-26 22:39:46 +0500

    create a user update class

---
+++ lib/Pages/update_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateUser extends StatefulWidget {
+  const UpdateUser({super.key});
+
+  @override
+  State<UpdateUser> createState() => _UpdateUserState();
+}
+
+class _UpdateUserState extends State<UpdateUser> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
