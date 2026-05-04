911c85092336eed1b6d0d35d38f2be5d53966b6b
aliraza055 <alirazamaqbool055@gmail.com>
2026-05-04 19:56:16 +0500

    changes

--- lib/Models/auth_model.dart
+++ lib/Models/auth_model.dart
@@ -39,7 +39,7 @@ class AuthModel {
             ).toMap(),
           );
       ToastError().showToast(
-        message: 'Create user successful!',
+        message: 'Create user successfully!',
         bgColor: Colors.green,
       );
     } on FirebaseAuthException catch (e) {
